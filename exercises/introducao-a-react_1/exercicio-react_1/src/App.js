c9ca70420f302492babfe37d5c6ccd6f7da07752
Thiago Gomes <thiagog.macedom@gmail.com>
2021-05-25 00:05:16 -0300

    finalizacao exercicio parte 1

--- exercises/introducao-a-react_1/exercicio-react_1/src/App.js
+++ exercises/introducao-a-react_1/exercicio-react_1/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+// import Task from 'Task';
+import React from 'react'
 
-function App() {
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li> Tarefa: {value}</li>
   );
+}
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+tarefas.push('Dormir')
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Tarefas diárias</h1>
+        <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
